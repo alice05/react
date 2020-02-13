cab5b4bb540b26e70408656c74f0b04f64d2818e
Santosh Kc <santosh@artup.io>
2020-02-13 21:29:05 +0545

    section 3 completed

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import styles from './MyStyle.module.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -44,20 +45,7 @@ class App extends Component {
   }
 
   render() {
-
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-
+    let btnClass = [styles.Button];
     let persons = null;
     if (this.state.showPerson) {
       persons = (
@@ -71,22 +59,22 @@ class App extends Component {
           })}
         </div>
       );
+      btnClass.push(styles.Red);
     }
 
     let classes = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      classes.push(styles.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      classes.push(styles.bold);
     }
 
-
     return (
-        <div className="App">
+        <div className={styles.App}>
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working</p>
-          <button className="button" onClick={this.togglePersonHandler}>Switch Name</button>
+          <button className={btnClass.join(' ')} onClick={this.togglePersonHandler}>Switch Name</button>
           {persons}
         </div>
     );
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,31 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
+import classes from './Person.module.css';
 
-const styledDiv = styled.div`
-    width: 60%;
-    margin: auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 400px;
-    }
-`;
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '400px'
-        }
-    }
     return (
-        <styledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}> I'm {props.name} and I am {props.age} years old</p>
             <input type="text" onChange={props.changed} value={props.name} />
             <p>{props.children}</p>
-        </styledDiv>
+        </div>
     )
 };
 
